e0ee6eef8e2f0e31bd1d2c1a9f2713b0f974387b
mikebender <mikebender@deephaven.io>
2026-01-23 12:56:35 -0500

    Refactor: Extract Deephaven logo into separate component
    - Created DeephavenLogo.tsx component for better reusability
    - Updated TodoList to import and use the new logo component

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Todo from "./Todo";
 import NewTodoForm from "./NewTodoForm";
+import DeephavenLogo from "./DeephavenLogo";
 import { v4 as uuid } from "uuid";
 import "./TodoList.css";
 
@@ -52,18 +53,7 @@ function TodoList() {
       </ul>
       <NewTodoForm create={create} />{" "}
       <div className="logo-container">
-        <svg
-          viewBox="0 0 57 57"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className="deephaven-logo"
-        >
-          <rect y="38.1" width="18.9" height="18.9" fill="#4DCCFA" />
-          <rect x="18.9" width="18.9" height="18.9" fill="#4DCCFA" />
-          <rect x="18.9" y="38.1" width="18.9" height="18.9" fill="#FDD041" />
-          <rect x="37.8" y="19" width="18.9" height="18.9" fill="#F33666" />
-          <rect y="0" width="18.9" height="38.1" fill="#3A57E1" />
-        </svg>
+        <DeephavenLogo />
       </div>{" "}
     </div>
   );
